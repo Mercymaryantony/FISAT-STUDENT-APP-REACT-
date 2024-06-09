cb613e95d7e35234f327297f78b83785fa4dab38
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-09 22:11:36 +0530

    ALL COMPLETE

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar.jsx'
+import { redirect } from 'react-router-dom'
 
 const Add = () => {
+    const [data,setData]=useState(
+        {
+            "fn":"",
+            "ln":"",
+            "cl":"",
+            "dob":"",
+            "co":"",
+            "mo":"",
+            "em":"",
+            "ad":""
+        }
+    )
+    const inputhandler = (event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue = ()=>{
+        console.log(data)
+    }
   return (
     <div>
         <Navbar/>
@@ -11,38 +30,38 @@ const Add = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">FIRST NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='fn' value={data.fn} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">LAST NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='ln' value={data.ln} onChange={inputhandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">COLLEGE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='cl' value={data.cl} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">DOB</label>
-                            <input type="date" name="" id="" className="form-control" />
+                            <input type="date" name="dob" value={data.dob} onChange={inputhandler} id="" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">COURSE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"  name='co' value={data.co} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">MOBILE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='mo' value={data.mo} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">EMAIL</label>
-                            <input type="text" className="from-control" />
+                            <input type="text" className="from-control" name='em' value={data.em} onChange={inputhandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">ADDRESS</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="ad" value={data.ad} onChange={inputhandler} id="" className="form-control"></textarea>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-primary">ADD</button>
+                            <button className="btn btn-primary" onClick={readValue}>ADD</button>
                         </div>
                     </div>
                 </div>
